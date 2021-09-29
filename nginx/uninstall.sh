15e05597c8955a62cf22fe5fa7ce046756f2d513
1379Pudge <EternalNightYeah@yeah.net>
2021-09-30 07:56:45 +0800

    fix bugs

--- nginx/uninstall.sh
+++ nginx/uninstall.sh
@@ -2,10 +2,10 @@
 container_name="1379-nginx"
 image_name="1379/nginx"
 
-echo ************正在停止容器 -> %name%
+echo ************正在停止容器
 docker stop ${container_name}
-echo ************正在卸载容器 -> %name%
+echo ************正在卸载容器
 docker rm ${container_name}
-echo ************正在卸载镜像 -> %image%
+echo ************正在卸载镜像
 docker rmi ${image_name}
 echo *************已经清理完毕请按任意键关闭窗口 -!-
